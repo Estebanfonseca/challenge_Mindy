4e13cd709510eae3aca9bea7bcd37830bc44fd81
Estebanfonseca <estebanp57@hotmail.com>
2022-10-26 17:15:17 -0500

    imagenes

--- js/juguetes.js
+++ js/juguetes.js
@@ -108,3 +108,5 @@ function prodNotFound() {
 ///// hasta acá va Laila
 //Nico tarea: cambiar el color de productos con 3 unidades para venderlos más rápido (cambiar background de la card)
 // ordenar las cards para que estén primero
+
+
