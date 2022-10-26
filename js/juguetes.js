aab2093e5f743835fbfe1f9e17827ca37acc5584
Estebanfonseca <estebanp57@hotmail.com>
2022-10-26 16:14:21 -0500

    filtros

--- js/juguetes.js
+++ js/juguetes.js
@@ -1,64 +1,45 @@
-let inputText= document.getElementById("text-search-js")
-console.log(inputText)
-let checkboxes= document.getElementById("filtro")
-let container= document.getElementById("container")
+let inputText = document.getElementById("text-search-js")
+let checkboxes = document.getElementById("filtro")
+let container = document.getElementById("container")
+let inputsNumber = document.getElementById("botonRango")
 
 
-async function mindyJuguetes(){
-   try{ 
-    let datos= await fetch("https://apipetshop.herokuapp.com/api/articulos?tipo=Juguete")
-    let products = await datos.json()
-    let productos= products.response
-    
-    let juguetes= tipo(productos,"Juguete")
-    console.log(juguetes[0].nombre);
-    
-    cardCreator (juguetes)
-    // filtradoCheck(juguetes)
-    checkboxes.addEventListener("change",(e)=>{
-        container.innerHTML = ""
-        cardCreator(filtrado(juguetes))
-    // let text= (filtroText(inputText.value,juguetes))
+async function mindyJuguetes() {
+    try {
+        let datos = await fetch("https://apipetshop.herokuapp.com/api/articulos?tipo=Juguete")
+        let products = await datos.json()
+        let productos = products.response
+        let juguetes = tipo(productos, "Juguete")
+        cardCreator(juguetes)
+        // filtradoCheck(juguetes)
 
-    //       if(text.length !==0){
-    //             container.innerHTML=""
-    //             cardCreator(text)
-    //         } else {
-        
-    //             prodNotFound()
-    //         }
-            
-   })
+        inputsNumber.addEventListener("click", () => { range(juguetes) })
 
+        inputText.addEventListener("keyup", () => {
+             filtroText(range(juguetes), inputText.value)
 
-
-   inputText.addEventListener("keyup", () => {
-   console.log(filtroText(inputText.value,juguetes))
-    
-    
-
-    
-})
-    // filtrado(productos)
-}
-catch(error){
-    console.log(error);
-}
+        })
+        // filtrado(productos)
+    }
+    catch (error) {
+        console.log(error);
+    }
 
 }
 mindyJuguetes()
 /// funcion que filtra por tipo de producto
-function tipo(array,propiedad){
+function tipo(array, propiedad) {
 
-        let filtrado= array.filter(array=>array.tipo== [propiedad])
-         return (filtrado)   
-  
-  }
+    let filtrado = array.filter(array => array.tipo == [propiedad])
+    return (filtrado)
+
+}
 
 // function imprimir cards de productos
-function cardCreator (array){
-    array.forEach(card=>{
-    document.getElementById("container").innerHTML += `
+function cardCreator(array) {
+    if (array.length > 0) {
+        array.forEach(card => {
+            document.getElementById("container").innerHTML += `
         
     <article class=" card rounded-4 p-0 pb-5 " style="width: 18rem;" id="card">
     
@@ -76,82 +57,53 @@ function cardCreator (array){
 
 
 `})
+    } else {
+        document.getElementById("container").innerHTML =`
+        <h2>No se puede encontrar productos con este nombre</h2>`
+    }
 }
-
-/// función para filtrar checkbox y buscador (asignado a Laila)
-function printFiltrado(){
- document.getElementById("filtro").innerHTML +=`
- 
-        <label class="form-check-label">
-            Desde $300 Hasta $400
-                <input class="form-check-input" type="checkbox" value="option1"> 
-            </label>
-            <label class="form-check-label"> 
-            Desde $400 Hasta $500
-                <input class="form-check-input" type="checkbox" value="option2">
-            </label>
-              <label class="form-check-label">
-              Desde $500
-                <input class="form-check-input" type="checkbox" value="option3">
-            </label>
-           
-          
-    `
-}
-printFiltrado()
-
-function filtrado(array) {
-    
-    //filtrado de precios
-    let productoOption1= (array.filter(e=> e.precio >= 300 && e.precio <= 400))
-    let productoOption2= (array.filter(e=> e.precio >= 401 && e.precio <= 500))
-    let productoOption3= (array.filter(e=> e.precio >= 501))
-    //filtrado de checks
-    let checked = [...document.querySelectorAll('input[type="checkbox"]')].filter( check => check.checked)
-    let checkSelected= checked.map(checked => checked.value)
-    if (checkSelected =="option1"){
-        console.log(productoOption1);
-
-        return productoOption1
-    } else if (checkSelected=="option2"){
-        console.log(productoOption2);
-        return productoOption2
-    } else if (checkSelected=="option3"){
-        console.log(productoOption3);
-        return productoOption3
-    } else if(checkSelected.length===0){
+function range(array) {
+    document.getElementById("container").innerHTML = ""
+    let min = document.getElementById("minimo")
+    console.log(min)
+    let max = document.getElementById("maximo")
+    console.log(max.value)
+    let precioFiltrado = array.filter(e => (min.value <= e.precio && e.precio <= max.value))
+    console.log(precioFiltrado)
+    cardCreator(precioFiltrado)
+    if(precioFiltrado.length== 0){
+        document.getElementById("container").innerHTML = ""
+        cardCreator(array)
         return array
-    }  
-
-       
+    }
+    return array
 }
 
-function filtroText(array){
-    
-    let arrayFiltrado = array.filter(producto => producto.nombre.toLowerCase().includes(texto.toLowerCase()|| producto.descripcion.toLowerCase().includes(texto.toLowerCase()|| producto.precio == texto)))
-   
+
+
+
+function filtroText(array, texto) {
+    document.getElementById("container").innerHTML = ""
+    console.log(inputText.value)
+    let arrayFiltrado = array.filter(producto => producto.nombre.toLowerCase().includes(texto.toLowerCase() || producto.descripcion.toLowerCase().includes(texto.toLowerCase()) || producto.precio == texto))
+    cardCreator(arrayFiltrado)
+    if(inputText.value.length== 0){
+        document.getElementById("container").innerHTML = ""
+        cardCreator(array)
+        return array
+    }
     return arrayFiltrado
 }
-// function textoSearch(texto, array) {
-        
-    
-//     let arrayFiltrado = array.filter(array => array.nombre.toLowerCase().includes(texto.toLowerCase()|| array.descripcion.toLowerCase().includes(texto.toLowerCase()|| array.precio == texto)))
-   
-//     return arrayFiltrado
-    
-   
 
-// }
+function prodNotFound() {
 
-function prodNotFound(){
-    
     container.innerHTML = `
     <div class="card ">
     <p class="card-text"> <span class="fw-bold">"${inputText.value}"</span> No encontramos un producto que se ajuste a tu búsqueda.. </p> 
    
   </div>
     `
-     
+
 }
 ///// hasta acá va Laila
 //Nico tarea: cambiar el color de productos con 3 unidades para venderlos más rápido (cambiar background de la card)
