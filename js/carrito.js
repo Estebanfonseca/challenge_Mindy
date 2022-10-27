ade3ecf20bba11109b3cf06962e4da13b957dfe5
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-10-27 16:24:40 -0300

    agrego tabla carrito y extraigo datos localStorage

--- js/carrito.js
+++ js/carrito.js
@@ -1,3 +1,24 @@
 let productoSeleccionado = localStorage.getItem("producto")
 productoSeleccionado = JSON.parse(productoSeleccionado)
 console.log(productoSeleccionado)
+
+let contenedorTabla = document.getElementById("tbody")
+
+
+
+function imprimirTabla (array,contenedor){
+     array.forEach(e=>{
+        contenedor.innerHTML +=`
+         <tr>
+    <td>           </td>
+    <td class="text-light">${e.nombre}</td>
+    <td class="text-light">${e.stock}</td>
+    <td class="text-light">.-$ ${e.precio}</td>
+    <td class="text-light">${e.tipo}</td>
+    <td class="text-light">1</td>
+  </tr>
+        `
+    })
+
+}
+imprimirTabla(productoSeleccionado,contenedorTabla)
